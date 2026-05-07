bea8bc836262f221265a88d68bf17d8d6c3b73ed
Zyad Galal <zyadg@banquemisr.com>
2026-05-07 15:35:12 +0300

    Initial Commit

---
+++ JahezTask/JahezTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  JahezTaskApp.swift
+//  JahezTask
+//
+//  Created by Zyad Galal on 07/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct JahezTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JahezTaskTests/JahezTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  JahezTaskTests.swift
+//  JahezTaskTests
+//
+//  Created by Zyad Galal on 07/05/2026.
+//
+
+import Testing
+@testable import JahezTask
+
+struct JahezTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
